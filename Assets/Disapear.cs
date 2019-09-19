d69b48b5c939154e7cfbc185ca11d40df4b65f09
Lizardy-Wizard <54945431+Lizardy-Wizard@users.noreply.github.com>
2019-09-18 21:39:35 -0500

    Homework Week 5

---
+++ Assets/Disapear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disapear : MonoBehaviour
+
+{
+
+    float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+        Debug.Log("I am working");
+        time += Time.deltaTime;
+        if(time > 2){
+           // Destroy(gameObject);
+        }
+        //
+        }
+    }
+}
---
+++ Assets/Play_Sound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Sound : MonoBehaviour
+{
+    AudioSource aud;
+    public AudioClip clip;
+    public int totalPickups = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = this.GetComponent<AudioSource>();
+        this.GetComponent<Collider>().isTrigger = true;    
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Something Hit Me!");
+        if(other.gameObject.CompareTag("Player")){
+            aud.PlayOneShot(clip);
+            totalPickups++;
+            //System.Threading.Thread.Sleep(1000);
+            //Destroy(this.gameObject);
+            StartCoroutine(Wait ());
+        } else {
+            Debug.Log("It Wasn't the Player...");
+        }
+    }
+
+    IEnumerator  Wait(){
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+}
